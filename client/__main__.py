f5e60efcdbfee1fb5d1be86a93fec190ab288e81
Edgar Vallejo Curti <bam_vllj98@outlook.com>
2022-05-05 17:28:03 -0500

    styling docs

--- client/__main__.py
+++ client/__main__.py
@@ -1,14 +1,10 @@
 from client import Client
 
 def main():
-    
     sock = Client()
     sock.sign()
     input('Click enter to start sending the file...')
     sock.send_file('client/message.txt')
-    
-    #server = client.create_server()
-    #client.send_file(server, 'client/message.txt')
     
 if __name__ == '__main__':
     main()
--- server/__main__.py
+++ server/__main__.py
@@ -1,9 +1,6 @@
 from server import Server
 
 def main():
-    #sock = server.create()
-    #server.run(sock, 'server/file.txt')
-    
     sock = Server()
     input('Click enter to read the sign...')
     sock.read_sign()
